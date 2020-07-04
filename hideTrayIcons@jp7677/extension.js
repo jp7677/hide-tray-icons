0ab866f3509d8ea87a7ea9b006eca9b14bff7aec
Jens Peters <jp7677@gmail.com>
2020-07-04 12:32:38 +0200

    Remove boilerplate code.

--- hideTrayIcons@jp7677/extension.js
+++ hideTrayIcons@jp7677/extension.js
@@ -1,35 +1,16 @@
-const Main				= imports.ui.main;
-const Shell				= imports.gi.Shell;
-const Me				= imports.misc.extensionUtils.getCurrentExtension();
-const System			= imports.system;
+const System = imports.system;
+const Main = imports.ui.main;
+const Shell = imports.gi.Shell;
 
-var HideTrayIconsClass = new imports.lang.Class({
-	Name: 'Hide Tray Icons',
+let tray;
 
-	_init() {
-		this.tray       = new Shell.TrayManager();
-		this.tray.connect('tray-icon-added', () => {});
-		this.tray.manage_screen(Main.panel);
-	},
-
-	_destroy() {
-		this.tray = null;
-		System.gc();
-	}
-});
-
-let TrayIcons;
-
-class Extension {
-    enable() {
-		TrayIcons = new HideTrayIconsClass();
-    }
-
-    disable() {
-		TrayIcons._destroy();
-	}
+function enable() {
+	tray = new Shell.TrayManager();
+	tray.connect("tray-icon-added", () => {});
+	tray.manage_screen(Main.panel);
 }
 
-function init() {
-    return new Extension();
+function disable() {
+	tray = null;
+	System.gc();
 }
